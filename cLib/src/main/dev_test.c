dde3edeb5d0c917dc8475c2532b8273fb8edf26e
falcon <billnosdname@gmail.com>
2016-11-17 00:44:26 +0000

    change the path of config.h created by autoconf

--- cLib/src/main/dev_test.c
+++ cLib/src/main/dev_test.c
@@ -1,10 +1,19 @@
 #include <fclib_printf.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 int main(){
 
-	FC_DEBUG("FC_DEBUG");
-	FC_INFO("info");
-	FC_ERROR("FC_ERROR: %d, %d, %d", 1, 2, 3);
-	return 0;
+	uint64_t x = 1;
+	uint32_t y = -1;
+
+	printf("x: %"PRId64", y: %"
+			PRId32"\n", x, y);
+
+
+//	FC_DEBUG("FC_DEBUG");
+//	FC_INFO("info");
+//	FC_ERROR("FC_ERROR: %d, %d, %d", 1, 2, 3);
+//	return 0;
 
 }
