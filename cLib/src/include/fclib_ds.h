af9c4dc513b9d45b11e7fc349e67152323c2320a
falcon <billnosdname@gmail.com>
2016-11-18 15:40:02 +0000

    minous fix on print hashtable

--- cLib/src/include/fclib_ds.h
+++ cLib/src/include/fclib_ds.h
@@ -56,19 +56,20 @@ typedef LIST_HEAD(fc_l_i_h, fc_list_i_t) fc_list_i_h;
 /* free the space of table ht */
 #define FC_HT_FREE(ht)		hdestroy_r(ht)
 /* get hashing value from pointer */
-#define HT_HT_H(ptr) (*(int*)ptr)
+#define HT_HT_H(ptr) (*(long*)ptr)
 /* get hashed element's key from pointer */
 #define HT_HT_K(ptr) ( ((ENTRY *) (ptr + sizeof(long)))->key )
 /* get hashed element's value from pointer */
 #define HT_HT_V(ptr) ( ((ENTRY *) (ptr + sizeof(long)))->data )
 /* get next element's address from pointer */
 #define HT_HT_NEXT(ptr) (ptr + sizeof(long) + sizeof(struct entry))
-/* print all the elements in hash table */
-#define FC_HT_PRINT(ht) do { \
+/* print all the elements in hash table, v_type: type of element value, fmt: formate of value */
+#define FC_HT_PRINT(ht, v_type, v_fmf) do { \
 		void *ptr = ht->table;	\
+		unsigned int i;			\
 		for (i =  0; i < ht->size; ++i){ \
 			if(HT_HT_H(ptr) != 0){ 	\
-			printf("Hash:%d\tkey: %s\t\tValue: %d\n", HT_HT_H(ptr), HT_HT_K(ptr), *((int *)(HT_HT_V(ptr)))); \
+			printf("Index:%u\tHash:%li\tKey: %s\t\tValue: %"fmf"\n",i, HT_HT_H(ptr), HT_HT_K(ptr), *((v_type *)(HT_HT_V(ptr)))); \
 			}	\
 			ptr = HT_HT_NEXT(ptr);	\
 		}	\
